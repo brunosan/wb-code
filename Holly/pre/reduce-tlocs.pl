15da21bc6a891cab574ea0ecfb4a41bd0c0e83ff
Bruno Sanchez-Andrade Nuno <brunosan@gmail.com>
2015-03-05 16:32:36 -0500

    handling timestamp reduce. not finished.

--- Holly/pre/reduce-tlocs.pl
+++ Holly/pre/reduce-tlocs.pl
@@ -5,11 +5,19 @@ use strict;
 my %ocurr;
 while (<>) {
     chomp;
-    my ($field, $count) = split / /;
-    $ocurr{$field} = 0 if (not exists $ocurr{$field});
-    $ocurr{$field} += $count;
+    my @list = split /,/;
+    my $imei=$list[0];
+    splice @list 1;
+    printf @list,"\n"; 
+    #for (substr @list 1)-> $time, $lat, $lon {
+    #say "$time, $lat, $lon";
+	    #}
+    #($field, $count) = split / /;
+    #$ocurr{$field} = 0 if (not exists $ocurr{$field});
+    #$ocurr{$field} += $count;
 }
 
+printf "end";
 my $total = 0;
 foreach my $field (sort keys %ocurr) {
     $total += $ocurr{$field};
